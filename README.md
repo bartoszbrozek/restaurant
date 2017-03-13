8a9af2137bf22033e7c436ba39ae33a7afd1f069
Barteł <bartekbrozek@gmail.com>
2017-03-13 22:34:12 +0100

    Inital Commit! Yes!

---
+++ README.md
@@ -0,0 +1,4 @@
+restaurant
+==========
+
+A Symfony project created on March 13, 2017, 9:22 pm.
---
+++ src/AdminBundle/Controller/DefaultController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AdminBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/admin")
+     */
+    public function indexAction()
+    {
+        return $this->render('AdminBundle:Default:index.html.twig', [
+            'page_title' => 'Statistics'
+        ]);
+    }
+}
