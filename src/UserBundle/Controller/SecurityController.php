324bb2559baa373fbd8c49032bce8f2904651ed3
Barteł <bartekbrozek@gmail.com>
2017-04-02 23:19:19 +0200

    Added very simple user auth

---
+++ src/UserBundle/Controller/SecurityController.php
@@ -0,0 +1,31 @@
+<?php
+namespace UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Security\Core\SecurityContextInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class SecurityController extends Controller
+{
+
+    /**
+     * @Route("/admin/login", name="UserBundle:login")
+     */
+    public function loginAction(Request $request)
+    {
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('AdminBundle:Security:login.html.twig', array(
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ));
+    }
+}
