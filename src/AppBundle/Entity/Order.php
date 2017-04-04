a4d57cf15780e763eb2d146a6847b9a2346c5001
Barteł <bartekbrozek@gmail.com>
2017-04-04 22:55:49 +0200

    Tried 5 mins hard to impement ManyToMany.

--- src/AppBundle/Entity/Order.php
+++ src/AppBundle/Entity/Order.php
@@ -1,7 +1,33 @@
 <?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use UserBundle\Entity\User;
+
 /**
- * Created by PhpStorm.
- * User: barte
- * Date: 04.04.2017
- * Time: 22:31
+ * Order
+ * @Entity
  */
+class Order
+{
+
+    /**
+     * @ManyToMany(targetEntity="UserBundle:User")
+     * @JoinTable(name="user_product",
+     *     joinColumns={@JoinColumn(name="product_id", referencedColumnName="id")},
+     *     inverseJoinColumns={@JoinColumn(name="order_id"), referencedColumnName="id"}}
+     *     )
+     */
+    protected $purchasers;
+
+    public function __construct()
+    {
+        $this->purchasers = new ArrayCollection();
+    }
+
+    public function getPurchasers()
+    {
+        return $this->purchasers;
+    }
+}
